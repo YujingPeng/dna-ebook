1f157a3057f74ce0f6b5e00c77ba113c6370c3ae
Manweill <mic.liangwenwei@foxmail.com>
2017-07-04 00:04:39 +0800

    add new book source `新笔趣阁`

--- src/env.js
+++ src/env.js
@@ -59,35 +59,6 @@ const kanshuge = {
   }
 }
 
-const biquge = {
-  host: 'http://www.biquge.com.tw',
-  sid: '10048850760735184192',
-  encode: 'gbk',
-  name: '笔趣阁1',
-  info: {
-    name: '#info > h1',
-    author: {
-      selector: '#info > p:nth-child(2)',
-      pattern: '作    者：'
-    },
-    updateAt: {
-      selector: '#info > p:nth-child(4)',
-      pattern: '最后更新：'
-    },
-    latestChapter: {
-      selector: '#info > p:nth-child(5) > a',
-      pattern: '最新章节：'
-    },
-    desc: '#intro'
-  },
-  thumbImage: '#fmimg > img',
-  searchThumbImage: (host, parm1, parm2) => `${host}/files/article/image/${Number(parm1) + 1}/${parm2}/${parm2}.jpg`,
-  chapterMenuUri: null,
-  chapterMenu: '#list > dl > dd > a',
-  firstChapterIndex: 9,
-  content: '#content'
-}
-
 const booktxt = {
   host: 'http://www.booktxt.net',
   name: '顶点文学 (推荐) ',
@@ -146,7 +117,65 @@ const booktxt = {
   }
 }
 
+const xxbiquge = {
+  host: 'http://www.xxbiquge.com',
+  name: '新笔趣阁',
+  encode: 'utf-8',
+  content: '#content',
+  info: {
+    name: '#info > h1',
+    author: {
+      selector: '#info > p:nth-child(2)',
+      pattern: '作    者：'
+    },
+    updateAt: {
+      selector: '#info > p:nth-child(4)',
+      pattern: '最后更新：'
+    },
+    latestChapter: {
+      selector: '#info > p:nth-child(5) > a',
+      pattern: '最新章节：'
+    },
+    desc: '#intro'
+  },
+  thumbImage: '#fmimg img',
+  searchThumbImage: (host, parm1, parm2) => `${host}/files/article/image/${Number(parm1) + 1}/${parm2}/${parm2}.jpg`,
+  chapterMenuUri: null,
+  chapterMenu: '#list > dl > dd > a',
+  firstChapterIndex: 0,
+  searchUri: 'http://zhannei.baidu.com/cse/search?s=5199337987683747968&q=',
+  searchItem: '#results div.result-item',
+  searchInfo: {
+    name: 'h3>a',
+    uri: {
+      selector: 'h3>a',
+      attr: 'href'
+    },
+    thumbImage: {
+      selector: 'img',
+      attr: 'src'
+    },
+    author: {
+      selector: '.result-game-item-info>p:nth-child(1)',
+      pattern: '作者：'
+    },
+    type: {
+      selector: '.result-game-item-info>p:nth-child(2)',
+      pattern: '类型：'
+    },
+    updateAt: {
+      selector: '.result-game-item-info>p:nth-child(3)',
+      pattern: '更新时间：'
+    },
+    latestChapter: {
+      selector: '.result-game-item-info>p:nth-child(4)',
+      pattern: '最新章节：'
+    },
+    desc: '.result-game-item-desc'
+  }
+}
+
 export const rules = {
   'booktxt.net': booktxt,
-  'kanshuge.la': kanshuge
+  'xxbiquge.com': xxbiquge
 }
