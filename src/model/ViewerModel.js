4d73a33524ab5ce6a4d05571baae14526930e850
Manweill <mic.liangwenwei@foxmail.com>
2017-07-12 16:38:34 +0800

    optimize  words width

--- src/model/ViewerModel.js
+++ src/model/ViewerModel.js
@@ -13,7 +13,7 @@ const LINE_INDENT = '        '
 const LINE_WRAP = '\n'
 
 function zhcnCode (s) {
-  return /[\u3400-\u4DB5\u4E00-\u9FA5\u9FA6-\u9FBB\uF900-\uFA2D\uFA30-\uFA6A\uFA70-\uFAD9\uFF00-\uFFEF\u2E80-\u2EFF\u3000-\u303F\u31C0-\u31EF\u2F00-\u2FDF\u2FF0-\u2FFF\u3100-\u312F\u31A0-\u31BF\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\u4DC0-\u4DFF\uA000-\uA48F\uA490-\uA4CF\u2800-\u28FF\u3200-\u32FF\u3300-\u33FF\u2700-\u27BF\u2600-\u26FF\uFE10-\uFE1F\uFE30-\uFE4F]/.test(s)
+  return /[\u3400-\u4DB5\u4E00-\u9FA5\u9FA6-\u9FBB\uF900-\uFA2D\uFA30-\uFA6A\uFA70-\uFAD9\uFF00-\uFFEF\u2E80-\u2EFF\u3000-\u303F\u31C0-\u31EF\u2F00-\u2FDF\u2FF0-\u2FFF\u3100-\u312F\u31A0-\u31BF\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\uAC00-\uD7AF\u1100-\u11FF\u3130-\u318F\u4DC0-\u4DFF\uA000-\uA48F\uA490-\uA4CF\u2800-\u28FF\u3200-\u32FF\u3300-\u33FF\u2700-\u27BF\u2600-\u26FF\uFE10-\uFE1F\uFE30-\uFE4F，。？：；’‘！”“、]/.test(s)
 }
 
 function HalfCode (s) {
@@ -97,7 +97,7 @@ class ChapterModel {
   list = []
 
   @observable
-  pagers = [{ key: 'page0', context: '正在加载...' }, { key: 'page1', context: '正在加载...' }]
+  pagers = [{ key: 'page0', context: '' }, { key: 'page1', context: '' }]
 
   @observable
   total = 1
